ace4244d442c935d9c542a1fa8e9597b190a66fe
hong <574231288@qq.com>
2024-05-15 16:32:37 +0800

    采用Spring IOC实现了依赖注入。完成了 Dao  JdbcUtils 的重构.

--- src/cn/ssm/dao/ProductDaoImpl.java
+++ src/cn/ssm/dao/ProductDaoImpl.java
@@ -2,54 +2,71 @@ package cn.ssm.dao;
 // 数据访问层,主要完成Product数据库操作
 
 import cn.ssm.model.Product;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.JdbcTemplate;
 
+import java.math.BigDecimal;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
+ //  XML --> dtd
+public class ProductDaoImpl
+{
 
-public class ProductDaoImpl extends BaseDaoImpl<Product>
-{   // 体现封装思想
+
+    private JdbcTemplate jdbcTemplate = null;
+
+    public void setJdbcTemplate(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
+    public ProductDaoImpl(){
+        System.out.println("ProductDaoImpl.ProductDaoImpl");
+    }
+
+    // 体现封装思想
     public int save(Product product){  // model类对数据进行封装
         String sql = "insert into product (name,price,remark) values (?,?,?)";
-//        Object[] param
-        return super.update(sql,new Object[]{product.getName(),product.getPrice(),product.getRemark()});
+        return jdbcTemplate.update(sql,new Object[]{product.getName(),product.getPrice(),product.getRemark()});
     }
 
     public int delete(int id){
         String sql = "delete from product where id = ?";
-        return super.update(sql,new Object[]{id});
+        return jdbcTemplate.update(sql,new Object[]{id});
     }
 
     public int update(Product product){
         String sql = "update product set name =?,price=?,remark=? where id=?";
-        return super.update(sql,new Object[]{product.getName(),product.getPrice(),
+        return jdbcTemplate.update(sql,new Object[]{product.getName(),product.getPrice(),
                 product.getRemark(),product.getId()});
     }
 
     public List<Product> queryByName(String keyword){
         String sql = "select * from product where name like ?";
-        return super.query(sql,new Object[]{"%" + keyword + "%"},Product.class);
+//        return super.query(sql,new Object[]{"%" + keyword + "%"},Product.class);
+        return jdbcTemplate.query(sql,new BeanPropertyRowMapper<Product>(Product.class),
+                new Object[]{"%" + keyword + "%"});
     }
 
     public static void main(String[] args) {
-        ProductDaoImpl productDao = new ProductDaoImpl();
-//        productDao.delete(2);
-
-        // 数据应该从 前端 --> 业务逻辑 --> 数据访问
-//        Product product = new Product();
-//        product.setId(3);
-//        product.setName("Iphone6");
-//        product.setPrice(7888.99);
-//        product.setRemark("AI手机智能化,支持GPU");
-//        int res = productDao.update(product);
-        // 查询测试
-        List<Product> proList = productDao.queryByName("");
-        for(Product p:proList){
-            System.out.println(p);
-        }
+          // 加载Spring的配置文件
+        ApplicationContext context = new ClassPathXmlApplicationContext("spring-bean.xml");
+        ProductDaoImpl productDao = context.getBean("productDao", ProductDaoImpl.class);
+//        System.out.println(productDao);
+//        Product product = new Product();  // 此数据应该前端
+//        product.setName("运动手表");
+//        product.setPrice(new BigDecimal(3333.88));
+//        product.setRemark("Spring 插入测试");
+//        productDao.save(product);
+         for(Product product:productDao.queryByName("")){
+             System.out.println(product);
+         }
     }
 
 }
