df1c02fe4295851d5d8b05dc3138cfd8ec95ed84
hong <574231288@qq.com>
2024-05-15 10:25:04 +0800

    query查询实现，this指向的是当前调用的对象。例如：子类调用了父类的方法，那么在父类中的this指向的是子类。

--- src/cn/ssm/dao/BaseDaoImpl.java
+++ src/cn/ssm/dao/BaseDaoImpl.java
@@ -1,10 +1,19 @@
 package cn.ssm.dao;
 
+import cn.ssm.model.Product;
+
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
-public class BaseDaoImpl {
+public abstract class BaseDaoImpl<T> {
+
+    // 定义一个抽象方法,此方法传入ResultSet,子类根据自己的情况获取数据
+    protected abstract T getRowMapper(ResultSet rs) throws SQLException;
+
     // 插入、更新、删除 都统称为更新, 数组: 限大小,限类型
     protected int update(String sql,Object[] param){
         // 获取conn连接对象
@@ -31,4 +40,35 @@ public class BaseDaoImpl {
 
         }
     }
+
+    protected List<T> query(String sql,Object[] param){
+        List<T> tList = new ArrayList<T>();
+        // 获取conn连接对象
+        Connection connection = null;
+        PreparedStatement statement = null;
+        // 结果集 (对应数据表)
+        ResultSet rs = null;
+        // 预编辑SQL语句
+        try {
+            connection = JdbcUtils.getConnection();
+            statement = connection.prepareStatement(sql);
+            // 设置参数
+            for(int i=0;i<param.length;i++){
+                statement.setObject(i+1,param[i]);
+            }
+            rs = statement.executeQuery();
+            while(rs.next()){ // 当前行是记录行则返回是true
+                System.out.println("this:" + this);
+                T t = this.getRowMapper(rs);  // 此方法由子类实现
+                tList.add(t);
+            }
+            return tList;
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }finally {
+            JdbcUtils.close(connection,statement,rs);
+        }
+    }
+
+
 }
--- src/cn/ssm/dao/ProductDaoImpl.java
+++ src/cn/ssm/dao/ProductDaoImpl.java
@@ -10,7 +10,7 @@ import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
-public class ProductDaoImpl extends BaseDaoImpl
+public class ProductDaoImpl extends BaseDaoImpl<Product>
 {   // 体现封装思想
     public int save(Product product){  // model类对数据进行封装
         String sql = "insert into product (name,price,remark) values (?,?,?)";
@@ -30,53 +30,37 @@ public class ProductDaoImpl extends BaseDaoImpl
     }
 
     public List<Product> queryByName(String keyword){
-        List<Product> proList = new ArrayList<Product>();
         String sql = "select * from product where name like ?";
-        // 获取conn连接对象
-        Connection connection = null;
-        PreparedStatement statement = null;
-        // 结果集 (对应数据表)
-        ResultSet rs = null;
-        // 预编辑SQL语句
-        try {
-            connection = JdbcUtils.getConnection();
-            statement = connection.prepareStatement(sql);
-            statement.setString(1,"%" + keyword + "%");
-            rs = statement.executeQuery();
-            while(rs.next()){ // 当前行是记录行则返回是true
-                // product表中的记录采用product对象来存储
-                Product product = new Product();
-                product.setId(rs.getInt("id"));
-                product.setName(rs.getString("name"));
-                product.setPrice(rs.getDouble("price"));
-                product.setRemark(rs.getString("remark"));
-                product.setDate(rs.getDate("date"));
-                // 当前存储了记录的product对象,存储到List中
-                proList.add(product);
-            }
-            return proList;
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
-        }finally {
-            JdbcUtils.close(connection,statement,rs);
-        }
+        return super.query(sql,new Object[]{"%" + keyword + "%"});
     }
 
     public static void main(String[] args) {
         ProductDaoImpl productDao = new ProductDaoImpl();
-        productDao.delete(2);
+//        productDao.delete(2);
 
         // 数据应该从 前端 --> 业务逻辑 --> 数据访问
-        Product product = new Product();
-        product.setId(3);
-        product.setName("Iphone6");
-        product.setPrice(7888.99);
-        product.setRemark("AI手机智能化,支持GPU");
-        int res = productDao.update(product);
+//        Product product = new Product();
+//        product.setId(3);
+//        product.setName("Iphone6");
+//        product.setPrice(7888.99);
+//        product.setRemark("AI手机智能化,支持GPU");
+//        int res = productDao.update(product);
         // 查询测试
         List<Product> proList = productDao.queryByName("");
         for(Product p:proList){
             System.out.println(p);
         }
+    }
+
+    @Override
+    protected Product getRowMapper(ResultSet rs) throws SQLException {
+        // product表中的记录采用product对象来存储
+        Product product = new Product();
+        product.setId(rs.getInt("id"));
+        product.setName(rs.getString("name"));
+        product.setPrice(rs.getDouble("price"));
+        product.setRemark(rs.getString("remark"));
+        product.setDate(rs.getDate("date"));
+        return product;
     }
 }
